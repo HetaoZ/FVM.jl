58976fa83498c4434585bfce3e9f237eaaa37091
HetaoZ <iamzhtr@hotmail.com>
2020-12-21 04:22:23 +0800

    add a correct step

--- src/solver.jl
+++ src/solver.jl
@@ -63,20 +63,22 @@ function update_cells!(f::Fluid, rk::Int, dt::Float64)
     coeff = RK_COEFF[:, rk]
 
     @sync @distributed for id in CartesianIndices(f.rho)
-        if MK.betweeneq([f.x[id[1]], f.y[id[2]], f.z[id[3]]], f.point1, f.point2)
-            w = coeff[1] * f.w[:, id] + coeff[2] * f.wb[:, id] + coeff[3] * f.rhs[:, id] * dt
-
-            ## This correction may cause mass loss.
-            # w = correct_cell_w(w, f.para["gamma"]) 
-
-            f.w[:, id] = w
-
-            rho, u, e, p = w_to_status(w, f.para["gamma"]) 
-
-            f.rho[id] = rho
-            f.u[:,id] = u
-            f.e[id] = e
-            f.p[id] = p
+        if f.mark[id] > 0
+            if MK.betweeneq([f.x[id[1]], f.y[id[2]], f.z[id[3]]], f.point1, f.point2)
+                w = coeff[1] * f.w[:, id] + coeff[2] * f.wb[:, id] + coeff[3] * f.rhs[:, id] * dt
+    
+                ## This correction may cause mass loss.
+                w = correct_cell_w(w, f.para["gamma"]) 
+    
+                f.w[:, id] = w
+    
+                rho, u, e, p = w_to_status(w, f.para["gamma"]) 
+    
+                f.rho[id] = rho
+                f.u[:,id] = u
+                f.e[id] = e
+                f.p[id] = p
+            end
         end
     end
 end
@@ -84,50 +86,46 @@ end
 function update_rhs!(f::Fluid)
     @sync @distributed for id in CartesianIndices(f.rho)
         i, j, k = id[1], id[2], id[3]
-        if MK.betweeneq([f.x[i], f.y[j], f.z[k]], f.point1, f.point2)
-            rhs = zeros(Float64, 5)
-            ws = Array{Float64,2}(undef, 5, 5)
-            # axis 1
-            for jj in 1:5
-                sw = f.w[:, i+jj-3, j, k]
-                for ii = 1:5
-                    ws[ii, jj] = sw[ii]
-                end
-            end
-            rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 1) / f.d[1]
-
-            # axis 2
-            if f.realdim > 1
+        if f.mark[id] > 0
+            if MK.betweeneq([f.x[i], f.y[j], f.z[k]], f.point1, f.point2)
+                rhs = zeros(Float64, 5)
+                ws = Array{Float64,2}(undef, 5, 5)
+                # axis 1
                 for jj in 1:5
-                    sw = f.w[:, i, j+jj-3, k]
-                    for ii = 1:5
-                        ws[ii, jj] = sw[ii]
-                    end
+                    ws[:, jj] = f.w[:, i+jj-3, j, k]
                 end
-                rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 2) / f.d[2]  
-
-                # axis 3
-                if f.realdim > 2
+                rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 1) / f.d[1]
+    
+                # axis 2
+                if f.realdim > 1
                     for jj in 1:5
-                        sw = f.w[:, i, j, k+jj-3]
-                        for ii = 1:5
-                            ws[ii, jj] = sw[ii]
-                        end
+                        ws[:, jj] = f.w[:, i, j+jj-3, k]
                     end
-                    rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 3) / f.d[3] 
-                end  
-            end 
-            
-            f.rhs[:,id] = rhs
+                    rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 2) / f.d[2]  
+    
+                    # axis 3
+                    if f.realdim > 2
+                        for jj in 1:5
+                            ws[:, jj] = f.w[:, i, j, k+jj-3]
+                        end
+                        rhs += get_dflux!(f.para["reconst scheme"], ws, f.para, 3) / f.d[3] 
+                    end  
+                end 
+                
+                f.rhs[:,id] = rhs
+            end        
         end
     end    
 end 
 
 function correct_cell_w(w, gamma)
-    if sign.([w[1], w[end], pressure(w, gamma)]) == [-1.0, -1.0, -1.0]
-        # @warn "Negative mass density"
-        # println(w)
-    elseif w[1] < 0 || w[end] < 0 || pressure(w, gamma) < 0
+    # if sign.([w[1], w[end], pressure(w, gamma)]) == [-1.0, -1.0, -1.0]
+    #     # @warn "Negative mass density"
+    #     # println(w)
+    # elseif w[1] < 0 || w[end] < 0 || pressure(w, gamma) < 0
+    #     w = zeros(Float64, 5)
+    # end
+    if w[1] < 0 #|| w[end] < 0 || pressure(w, gamma) < 0
         w = zeros(Float64, 5)
     end
     return w
